7c2c170d604d4d1580cb2c8260f3c45370efc6e4
stngry330 <jamie.kapilivsky@gmail.com>
2017-12-26 15:39:42 -0600

    Moving Error_log files

--- scripts/Test scripts/testing file.py
+++ scripts/Test scripts/testing file.py
@@ -1,10 +1,3 @@
 import pickle
 import pandas as pd
-df = pd.DataFrame(columns=['error message', 'script', 'time_stamp'])
 
-#error_log = pickle.load(open("../data/Instagram_error_log.p", "rb"))
-# df = pd.DataFrame([[err, 'new FOLLOW script', str(datetime.datetime.now())]],
-#                   columns=['error message', 'script', 'time_stamp'])
-# error_log = error_log.append(df)
-#pickle.dump(df, open("../data/Instagram_error_log.p", "wb"))
-pickle.dump(df, open('../../data/Instagram_error_log.p', 'wb'))
